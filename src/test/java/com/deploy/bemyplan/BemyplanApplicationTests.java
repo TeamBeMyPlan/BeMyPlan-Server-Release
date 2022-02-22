102763b353274c79231573d360115a214e19babe
안준영 <zxzimin@naver.com>
2022-02-22 23:48:18 +0900

    initial commit :)

---
+++ src/test/java/com/deploy/bemyplan/BemyplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deploy.bemyplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BemyplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

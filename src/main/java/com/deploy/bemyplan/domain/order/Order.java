c8366c38cbbaba5cb6a7c9d3a900d5accf440443
안준영 <zxzimin@naver.com>
2022-02-24 01:30:38 +0900

    [FEAT] 주문 도메인 정보 설정

---
+++ src/main/java/com/deploy/bemyplan/domain/order/Order.java
@@ -0,0 +1,24 @@
+package com.deploy.bemyplan.domain.order;
+
+import com.deploy.bemyplan.domain.common.AuditingTimeEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Order extends AuditingTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long postId;
+
+    @Column(nullable = false)
+    private Long userId;
+}

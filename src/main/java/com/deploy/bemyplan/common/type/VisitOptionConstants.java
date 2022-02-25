bb9f78ce592de1fe2e830d0c6250160174357246
안준영 <zxzimin@naver.com>
2022-02-25 14:07:18 +0900

    [FEAT] Visit-Option 헤더값에 따라 받아오는 USER_ID 분기 / 예외처리

--- src/main/java/com/deploy/bemyplan/common/type/VisitOptionConstants.java
+++ src/main/java/com/deploy/bemyplan/common/type/VisitOptionConstants.java
@@ -6,8 +6,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class VisitOptionConstants {
 
-    public static final String VISIT_OPTION_HEADER = "VISIT_OPTION";
+    public static final String VISIT_OPTION_HEADER = "Visit-Option";
 
     public static final String MEMBERSHIP = "MEMBERSHIP";
     public static final String GUEST = "GUEST";
+
+    public static final Long GUEST_MODE = -1L;
 }
--- src/main/java/com/deploy/bemyplan/config/interceptor/LoginCheckHandler.java
+++ src/main/java/com/deploy/bemyplan/config/interceptor/LoginCheckHandler.java
@@ -1,5 +1,6 @@
 package com.deploy.bemyplan.config.interceptor;
 
+import com.deploy.bemyplan.common.exception.ErrorCode;
 import com.deploy.bemyplan.common.exception.model.UnAuthorizedException;
 import com.deploy.bemyplan.common.exception.model.ValidationException;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +12,7 @@ import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 
+import static com.deploy.bemyplan.common.exception.ErrorCode.VALIDATION_SESSION_EXCEPTION;
 import static com.deploy.bemyplan.common.type.VisitOptionConstants.*;
 import static com.deploy.bemyplan.config.session.SessionConstants.USER_ID;
 
@@ -21,8 +23,8 @@ public class LoginCheckHandler {
     private final SessionRepository<? extends Session> sessionRepository;
 
     Long getUserId(HttpServletRequest request) {
-        String visitOption = request.getHeader(VISIT_OPTION_HEADER);
         String sessionId = request.getHeader(HttpHeaders.AUTHORIZATION);
+        String visitOption = request.getHeader(VISIT_OPTION_HEADER);
 
         switch (visitOption) {
             case MEMBERSHIP:
@@ -33,11 +35,12 @@ public class LoginCheckHandler {
                         return userId;
                     }
                 }
-                throw new UnAuthorizedException(String.format("잘못된 세션 (%S) 입니다", sessionId));
+                throw new ValidationException(String.format("잘못된 세션 (%S) 입니다", sessionId), VALIDATION_SESSION_EXCEPTION);
             case GUEST:
-                return null;
+                return GUEST_MODE;
+            default:
+                throw new ValidationException(String.format("허용하지 않는 Visit-Option 헤더값 (%s) 입니다. [MEMBERSHIP 또는 GUEST]", visitOption));
         }
-        throw new ValidationException(String.format("허용하지 않는 VISIT_OPTION 헤더값 (%s) 입니다. [MEMBERSHIP 또는 GUEST]", visitOption));
     }
 
     private Session findSessionBySessionId(String sessionId) {

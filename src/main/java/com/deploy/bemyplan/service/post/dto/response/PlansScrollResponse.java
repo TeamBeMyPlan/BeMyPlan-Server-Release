29d14f460e0e7c5c744846521ba38f21ed0202fd
안준영 <zxzimin@naver.com>
2022-02-27 05:10:56 +0900

    [ADD] 여행일정 정보 반환 DTO / 여행일정과 함께 반환될 작성자 정보 DTO 추가

---
+++ src/main/java/com/deploy/bemyplan/service/post/dto/response/PlansScrollResponse.java
@@ -0,0 +1,58 @@
+package com.deploy.bemyplan.service.post.dto.response;
+
+import com.deploy.bemyplan.domain.common.collection.ScrollPaginationCollection;
+import com.deploy.bemyplan.domain.collection.UserOrderDictionary;
+import com.deploy.bemyplan.domain.collection.UserScrapDictionary;
+import com.deploy.bemyplan.domain.post.Post;
+import com.deploy.bemyplan.domain.user.User;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ToString
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PlansScrollResponse {
+
+    private static final long LAST_CURSOR = -1L;
+
+    private List<PlanInfoResponse> contents = new ArrayList<>();
+    private long nextCursor;
+
+    private PlansScrollResponse(List<PlanInfoResponse> contents, long nextCursor) {
+        this.contents = contents;
+        this.nextCursor = nextCursor;
+    }
+
+    public static PlansScrollResponse of(ScrollPaginationCollection<Post> plansScroll, UserScrapDictionary userScrapDictionary, UserOrderDictionary userOrderDictionary, User user) {
+        if (plansScroll.isLastScroll()) {
+            return newLastCursor(plansScroll.getCurrentScrollItems(), userScrapDictionary, userOrderDictionary, user);
+        }
+        return newCursorHasNext(plansScroll.getCurrentScrollItems(), userScrapDictionary, userOrderDictionary, user, plansScroll.getNextCursor().getId());
+    }
+
+    private static PlansScrollResponse newLastCursor(List<Post> plans, UserScrapDictionary userScrapDictionary, UserOrderDictionary userOrderDictionary, @NotNull User user) {
+        return newCursorHasNext(plans, userScrapDictionary, userOrderDictionary, user, LAST_CURSOR);
+    }
+
+    private static PlansScrollResponse newCursorHasNext(List<Post> plans, UserScrapDictionary userScrapDictionary, UserOrderDictionary userOrderDictionary, User user, long nextCursor) {
+        List<PlanInfoResponse> contents = plans.stream()
+                .map(plan -> PlanInfoResponse.of(plan, user, getScarpStatus(plan, userScrapDictionary), getOrderStatus(plan, userOrderDictionary)))
+                .collect(Collectors.toList());
+        return new PlansScrollResponse(contents, nextCursor);
+    }
+
+    private static boolean getScarpStatus(Post plan, UserScrapDictionary userScrapDictionary) {
+        return userScrapDictionary.existByPlanId(plan.getId());
+    }
+
+    private static boolean getOrderStatus(Post plan, UserOrderDictionary userOrderDictionary) {
+        return userOrderDictionary.existByPlanId(plan.getId());
+    }
+}

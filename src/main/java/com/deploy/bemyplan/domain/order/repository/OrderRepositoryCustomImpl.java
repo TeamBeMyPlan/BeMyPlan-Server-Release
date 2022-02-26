a85e34c26ba703c5730521c846b5feb353fa9e32
안준영 <zxzimin@naver.com>
2022-02-27 05:18:52 +0900

    [FEAT] 주어진 여행일정 범위 내에서 유저가 구매한 리스트 가져오기 (Repository)

---
+++ src/main/java/com/deploy/bemyplan/domain/order/repository/OrderRepositoryCustomImpl.java
@@ -0,0 +1,39 @@
+package com.deploy.bemyplan.domain.order.repository;
+
+import com.deploy.bemyplan.domain.order.Order;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import static com.deploy.bemyplan.domain.order.QOrder.order;
+
+@RequiredArgsConstructor
+public class OrderRepositoryCustomImpl implements OrderRepositoryCustom{
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<Order> findAllByIds(List<Long> orderIds) {
+        return queryFactory
+                .selectFrom(order).distinct()
+                .where(
+                        order.id.in(orderIds)
+                )
+                .orderBy(order.id.desc())
+                .fetch();
+    }
+
+    @Override
+    public List<Order> findByUserIdAndPlanIds(List<Long> planIds, Long userId) {
+        List<Long> orderIds = queryFactory
+                .select(order.id).distinct()
+                .from(order)
+                .where(
+                        order.userId.eq(userId),
+                        order.postId.in(planIds)
+                )
+                .fetch();
+        return findAllByIds(orderIds);
+    }
+}

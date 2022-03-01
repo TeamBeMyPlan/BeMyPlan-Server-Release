494a5db803df5fb9442ddc8562bf7b68c3246b8d
안준영 <zxzimin@naver.com>
2022-03-02 02:20:25 +0900

    [FIX] RetrievePlansRequest의 rcmnd 필드타입을 Boolean으로 변경

--- src/main/java/com/deploy/bemyplan/controller/plan/PlanRetrieveController.java
+++ src/main/java/com/deploy/bemyplan/controller/plan/PlanRetrieveController.java
@@ -17,6 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+import static com.deploy.bemyplan.domain.plan.RcmndStatus.*;
+import static java.lang.Boolean.*;
+
 @Validated
 @RequiredArgsConstructor
 @RestController
@@ -29,7 +32,7 @@ public class PlanRetrieveController {
     @GetMapping("/v1/plans")
     public ApiResponse<PlansScrollResponse> getPlans(@UserId Long userId, @Valid RetrievePlansRequest request,
                                                             @AllowedSortProperties({"id", "createdAt", "orderCnt"}) Pageable pageable) {
-        RcmndStatus rcmndStatus = request.isRcmnd() ? RcmndStatus.RECOMMENDED : RcmndStatus.NONE;
+        RcmndStatus rcmndStatus = request.getRcmnd() == TRUE ? RECOMMENDED : NONE;
         return ApiResponse.success(planRetrieveService.retrievePlans(userId, request.getSize(), request.getLastPlanId(), pageable, rcmndStatus));
     }
 }

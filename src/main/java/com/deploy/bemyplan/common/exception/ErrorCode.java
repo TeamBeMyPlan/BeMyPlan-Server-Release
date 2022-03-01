3b02cc42be353dbe81cd8711edd37b8115c8d2a7
안준영 <zxzimin@naver.com>
2022-03-02 02:55:28 +0900

    [ADD] Not Found Plan 에러코드 추가

--- src/main/java/com/deploy/bemyplan/common/exception/ErrorCode.java
+++ src/main/java/com/deploy/bemyplan/common/exception/ErrorCode.java
@@ -28,6 +28,7 @@ public enum ErrorCode {
     // 404 Not Found
     NOT_FOUND_EXCEPTION(NOT_FOUND, "존재하지 않습니다"),
     NOT_FOUND_USER_EXCEPTION(NOT_FOUND, "탈퇴하거나 존재하지 않는 유저입니다"),
+    NOT_FOUND_PLAN_EXCEPTION(NOT_FOUND, "존재하지 않는 여행일정입니다"),
 
     // 405 Method Not Allowed
     METHOD_NOT_ALLOWED_EXCEPTION(METHOD_NOT_ALLOWED, "지원하지 않는 메소드 입니다"),

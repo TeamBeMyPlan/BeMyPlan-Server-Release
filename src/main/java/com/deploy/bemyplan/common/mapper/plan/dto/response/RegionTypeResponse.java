7151a236adac9a810fdf5943f0200cd30444139c
안준영 <zxzimin@naver.com>
2022-03-02 01:29:51 +0900

    [FEAT] 여행지(Region) 목록을 가져오는 Mapper 클래스 추가

---
+++ src/main/java/com/deploy/bemyplan/common/mapper/plan/dto/response/RegionTypeResponse.java
@@ -0,0 +1,25 @@
+package com.deploy.bemyplan.common.mapper.plan.dto.response;
+
+import com.deploy.bemyplan.domain.plan.RegionType;
+import lombok.*;
+
+@ToString
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class RegionTypeResponse {
+
+    private RegionType region;
+    private String name;
+    private String thumbnailUrl;
+    private boolean isLocked;
+
+    public static RegionTypeResponse of(RegionType region) {
+        return new RegionTypeResponse(
+                region,
+                region.getRegionName(),
+                region.getThumbnailUrl(),
+                region.isLocked()
+        );
+    }
+}

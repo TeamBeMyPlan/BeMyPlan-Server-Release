1d0ff1e28787a0de12183e307a4a90b9b01a618b
안준영 <zxzimin@naver.com>
2022-03-02 02:58:28 +0900

    [FEAT] 여행일정 조회 시 Null 처리를 위한 Util Class 추가

---
+++ src/main/java/com/deploy/bemyplan/service/plan/PlanServiceUtils.java
@@ -0,0 +1,26 @@
+package com.deploy.bemyplan.service.plan;
+
+import com.deploy.bemyplan.common.exception.ErrorCode;
+import com.deploy.bemyplan.common.exception.model.NotFoundException;
+import com.deploy.bemyplan.domain.plan.Plan;
+import com.deploy.bemyplan.domain.plan.PlanRepository;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+import static com.deploy.bemyplan.common.exception.ErrorCode.NOT_FOUND_PLAN_EXCEPTION;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PlanServiceUtils {
+
+    @NotNull
+    public static Plan findPlanById(PlanRepository planRepository, Long planId) {
+        Plan plan = planRepository.findPlanById(planId);
+        if (Objects.isNull(plan)) {
+            throw new NotFoundException(String.format("존재하지 않는 일정 (%s) 입니다", planId), NOT_FOUND_PLAN_EXCEPTION);
+        }
+        return plan;
+    }
+}

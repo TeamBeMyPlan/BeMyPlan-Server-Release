d58a17d12cf87d67eabebe688d5550ff09f0e184
안준영 <zxzimin@naver.com>
2022-03-02 13:18:40 +0900

    [FEAT] 여행일정 조회 (fetchJoin Schedule) Repository 메서드 작성

--- src/main/java/com/deploy/bemyplan/service/plan/PlanServiceUtils.java
+++ src/main/java/com/deploy/bemyplan/service/plan/PlanServiceUtils.java
@@ -1,6 +1,5 @@
 package com.deploy.bemyplan.service.plan;
 
-import com.deploy.bemyplan.common.exception.ErrorCode;
 import com.deploy.bemyplan.common.exception.model.NotFoundException;
 import com.deploy.bemyplan.domain.plan.Plan;
 import com.deploy.bemyplan.domain.plan.PlanRepository;
@@ -18,6 +17,15 @@ public class PlanServiceUtils {
     @NotNull
     public static Plan findPlanById(PlanRepository planRepository, Long planId) {
         Plan plan = planRepository.findPlanById(planId);
+        if (Objects.isNull(plan)) {
+            throw new NotFoundException(String.format("존재하지 않는 일정 (%s) 입니다", planId), NOT_FOUND_PLAN_EXCEPTION);
+        }
+        return plan;
+    }
+
+    @NotNull
+    public static Plan findPlanByIdFetchJoinSchedule(PlanRepository planRepository, Long planId) {
+        Plan plan = planRepository.findPlanByIdFetchJoinSchedule(planId);
         if (Objects.isNull(plan)) {
             throw new NotFoundException(String.format("존재하지 않는 일정 (%s) 입니다", planId), NOT_FOUND_PLAN_EXCEPTION);
         }

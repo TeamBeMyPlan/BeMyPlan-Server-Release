aaf438940c125f8f49687dafc1d47714e4433339
안준영 <zxzimin@naver.com>
2022-03-14 22:39:07 +0900

    [FEAT] 내가 구매한 여행일정 조회 API Controller 개발 / DTO 매핑

---
+++ src/main/java/com/deploy/bemyplan/service/plan/dto/request/RetrieveMyPlansOrdered.java
@@ -0,0 +1,21 @@
+package com.deploy.bemyplan.service.plan.dto.request;
+
+import lombok.*;
+import org.jetbrains.annotations.Nullable;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
+@ToString
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class RetrieveMyPlansOrdered {
+
+    @Min(value = 1, message = "{common.size.min}")
+    @Max(value = 100, message = "{common.size.max}")
+    private int size;
+
+    @Nullable
+    private Long lastOrderId;
+}

a7d70ce2f314456556a68f57745dc16a61b5cfa8
안준영 <zxzimin@naver.com>
2022-04-19 00:30:40 +0900

    [REFACTOR] PlanRetrieveService 공통 부분 메서드 추출 리팩터링

--- src/main/java/com/deploy/bemyplan/service/plan/PlanRetrieveService.java
+++ src/main/java/com/deploy/bemyplan/service/plan/PlanRetrieveService.java
@@ -3,31 +3,31 @@ package com.deploy.bemyplan.service.plan;
 import com.deploy.bemyplan.domain.collection.AuthorDictionary;
 import com.deploy.bemyplan.domain.common.collection.ScrollPaginationCollection;
 import com.deploy.bemyplan.domain.plan.*;
-import com.deploy.bemyplan.domain.scrap.Scrap;
-import com.deploy.bemyplan.service.plan.dto.request.RetrieveMyPlanBookmarksRequestDto;
-import com.deploy.bemyplan.service.plan.dto.request.RetrieveMyPlansOrdered;
+import com.deploy.bemyplan.service.plan.dto.request.RetrieveMyBookmarkListRequestDto;
+import com.deploy.bemyplan.service.plan.dto.request.RetrieveMyOrderListRequestDto;
 import com.deploy.bemyplan.service.plan.dto.response.PlanDetailResponse;
-import com.deploy.bemyplan.service.plan.dto.response.PlanInfoResponse;
 import com.deploy.bemyplan.service.plan.dto.response.PlanPreviewResponse;
 import com.deploy.bemyplan.service.plan.dto.response.PlansScrollResponse;
-import com.deploy.bemyplan.domain.collection.UserOrderDictionary;
-import com.deploy.bemyplan.domain.collection.UserScrapDictionary;
+import com.deploy.bemyplan.domain.collection.OrderDictionary;
+import com.deploy.bemyplan.domain.collection.ScrapDictionary;
 import com.deploy.bemyplan.domain.order.OrderRepository;
 import com.deploy.bemyplan.domain.scrap.ScrapRepository;
 import com.deploy.bemyplan.domain.user.User;
 import com.deploy.bemyplan.domain.user.UserRepository;
+import com.deploy.bemyplan.service.plan.dto.response.SpotMoveInfoResponse;
 import com.deploy.bemyplan.service.user.UserServiceUtils;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
+@Transactional(readOnly = true)
 public class PlanRetrieveService {
 
     private final UserRepository userRepository;
@@ -35,34 +35,11 @@ public class PlanRetrieveService {
     private final ScrapRepository scrapRepository;
     private final OrderRepository orderRepository;
 
-    @Transactional(readOnly = true)
     public PlansScrollResponse retrievePlans(Long userId, int size, Long lastPlanId, Pageable pageable, RegionType region, RcmndStatus rcmndStatus) {
-        List<Plan> planWithNextCursor = planRepository.findPlansUsingCursor(size+1, lastPlanId, pageable, region, rcmndStatus);
-        ScrollPaginationCollection<Plan> plansCursor = ScrollPaginationCollection.of(planWithNextCursor, size);
-
-        AuthorDictionary authors = AuthorDictionary.of(planWithNextCursor, userRepository);
-
-        UserScrapDictionary userScrapDictionary = findScrapByUserIdAndPlans(userId, planWithNextCursor);
-        UserOrderDictionary userOrderDictionary = findOrderByUserIdAndPlans(userId, planWithNextCursor);
-
-        return PlansScrollResponse.of(plansCursor, userScrapDictionary, userOrderDictionary, authors);
+        List<Plan> planWithNextCursor = planRepository.findPlansUsingCursor(size + 1, lastPlanId, pageable, region, rcmndStatus);
+        return getPlanListWithPersonalStatusUsingCursor(planWithNextCursor, userId, size);
     }
 
-    private UserScrapDictionary findScrapByUserIdAndPlans(Long userId, List<Plan> plans) {
-        List<Long> planIds = plans.stream()
-                .map(Plan::getId)
-                .collect(Collectors.toList());
-        return UserScrapDictionary.of(scrapRepository.findByUserIdAndPlanIds(planIds, userId));
-    }
-
-    private UserOrderDictionary findOrderByUserIdAndPlans(Long userId, List<Plan> plans) {
-        List<Long> planIds = plans.stream()
-                .map(Plan::getId)
-                .collect(Collectors.toList());
-        return UserOrderDictionary.of(orderRepository.findByUserIdAndPlanIds(planIds, userId));
-    }
-
-    @Transactional(readOnly = true)
     public PlanPreviewResponse getPreviewPlanInfo(Long planId) {
         Plan plan = PlanServiceUtils.findPlanByIdFetchJoinSchedule(planRepository, planId);
         List<PreviewContent> previewContents = planRepository.findPreviewContentsByPlanId(plan.getId());
@@ -70,37 +47,52 @@ public class PlanRetrieveService {
         return PlanPreviewResponse.of(plan, previewContents);
     }
 
-    @Transactional(readOnly = true)
     public PlanDetailResponse getPlanDetailInfo(Long planId) {
         Plan plan = PlanServiceUtils.findPlanByIdFetchJoinSchedule(planRepository, planId);
         User user = UserServiceUtils.findUserById(userRepository, plan.getUserId());
-        List<DailySchedule> schedules = planRepository.findSchedulesByPlanId(planId);
-        return PlanDetailResponse.of(plan, user, schedules);
+
+        return PlanDetailResponse.of(plan, user);
     }
 
-    @Transactional(readOnly = true)
-    public PlansScrollResponse retrieveMyPlanBookmarks(RetrieveMyPlanBookmarksRequestDto request, Long userId, Pageable pageable) {
-        List<Plan> planWithNextCursor = planRepository.findMyPlanBookmarksUsingCursor(userId, pageable, request.getSize()+1, request.getLastScrapId());
-        ScrollPaginationCollection<Plan> plansCursor = ScrollPaginationCollection.of(planWithNextCursor, request.getSize());
+    public List<SpotMoveInfoResponse> getSpotMoveInfos(Long planId) {
+        Plan plan = PlanServiceUtils.findPlanById(planRepository, planId);
 
-        AuthorDictionary authors = AuthorDictionary.of(planWithNextCursor, userRepository);
-
-        UserScrapDictionary userScrapDictionary = findScrapByUserIdAndPlans(userId, planWithNextCursor);
-        UserOrderDictionary userOrderDictionary = findOrderByUserIdAndPlans(userId, planWithNextCursor);
-
-        return PlansScrollResponse.of(plansCursor, userScrapDictionary, userOrderDictionary, authors);
+        return plan.getSchedules().stream()
+                .map(SpotMoveInfoResponse::of)
+                .collect(Collectors.toList());
     }
 
-    @Transactional(readOnly = true)
-    public PlansScrollResponse retrieveMyPlansOrdered(RetrieveMyPlansOrdered request, Long userId, Pageable pageable) {
+    public PlansScrollResponse retrieveMyBookmarkList(RetrieveMyBookmarkListRequestDto request, Long userId, Pageable pageable) {
+        List<Plan> planWithNextCursor = planRepository.findMyPlanBookmarksUsingCursor(userId, pageable, request.getSize() + 1, request.getLastScrapId());
+        return getPlanListWithPersonalStatusUsingCursor(planWithNextCursor, userId, request.getSize());
+    }
+
+    public PlansScrollResponse retrieveMyOrderList(RetrieveMyOrderListRequestDto request, Long userId, Pageable pageable) {
         List<Plan> planWithNextCursor = planRepository.findMyPlansOrderedUsingCursor(userId, pageable, request.getSize() + 1, request.getLastOrderId());
-        ScrollPaginationCollection<Plan> plansCursor = ScrollPaginationCollection.of(planWithNextCursor, request.getSize());
+        return getPlanListWithPersonalStatusUsingCursor(planWithNextCursor, userId, request.getSize());
+    }
+
+    private PlansScrollResponse getPlanListWithPersonalStatusUsingCursor(List<Plan> planWithNextCursor, Long userId, int size) {
+        ScrollPaginationCollection<Plan> plansCursor = ScrollPaginationCollection.of(planWithNextCursor, size);
 
         AuthorDictionary authors = AuthorDictionary.of(planWithNextCursor, userRepository);
 
-        UserScrapDictionary userScrapDictionary = findScrapByUserIdAndPlans(userId, planWithNextCursor);
-        UserOrderDictionary userOrderDictionary = findOrderByUserIdAndPlans(userId, planWithNextCursor);
+        ScrapDictionary scrapDictionary = findScrapByUserIdAndPlans(userId, planWithNextCursor);
+        OrderDictionary orderDictionary = findOrderByUserIdAndPlans(userId, planWithNextCursor);
+        return PlansScrollResponse.of(plansCursor, scrapDictionary, orderDictionary, authors);
+    }
 
-        return PlansScrollResponse.of(plansCursor, userScrapDictionary, userOrderDictionary, authors);
+    private ScrapDictionary findScrapByUserIdAndPlans(Long userId, List<Plan> plans) {
+        List<Long> planIds = plans.stream()
+                .map(Plan::getId)
+                .collect(Collectors.toList());
+        return ScrapDictionary.of(scrapRepository.findByUserIdAndPlanIds(planIds, userId));
+    }
+
+    private OrderDictionary findOrderByUserIdAndPlans(Long userId, List<Plan> plans) {
+        List<Long> planIds = plans.stream()
+                .map(Plan::getId)
+                .collect(Collectors.toList());
+        return OrderDictionary.of(orderRepository.findByUserIdAndPlanIds(planIds, userId));
     }
 }

3495d109f8d1d54d5abf4af9ea7de576906d077b
안준영 <zxzimin@naver.com>
2022-04-19 19:38:15 +0900

    [FIX] SpringBootTest 주석 처리

--- src/test/java/com/deploy/bemyplan/BemyplanApplicationTests.java
+++ src/test/java/com/deploy/bemyplan/BemyplanApplicationTests.java
@@ -3,7 +3,7 @@ package com.deploy.bemyplan;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BemyplanApplicationTests {
 
 	@Test

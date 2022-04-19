0137bc2fa4dceb5344b1f2c1cba2f43a58ea033c
안준영 <zxzimin@naver.com>
2022-04-19 21:56:28 +0900

    [FIX] AWS ElastiCache ConfigureRedisAction.NO_OP

--- src/main/java/com/deploy/bemyplan/config/session/RedisSessionConfig.java
+++ src/main/java/com/deploy/bemyplan/config/session/RedisSessionConfig.java
@@ -1,10 +1,17 @@
 package com.deploy.bemyplan.config.session;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
 
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 60 * 60 * 24 * 30) // 30일
 @Configuration
 public class RedisSessionConfig extends AbstractHttpSessionApplicationInitializer {
+
+    @Bean
+    public ConfigureRedisAction configureRedisAction() {
+        return ConfigureRedisAction.NO_OP;
+    }
 }

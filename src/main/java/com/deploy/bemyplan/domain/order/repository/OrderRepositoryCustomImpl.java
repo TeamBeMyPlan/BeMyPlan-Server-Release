ac3045127f45f4bc8f329f9a47f3d5eaf979de35
안준영 <zxzimin@naver.com>
2022-05-12 01:23:05 +0900

    [FEAT] booleanExpression Objects.isNull() 사용

--- src/main/java/com/deploy/bemyplan/domain/order/repository/OrderRepositoryCustomImpl.java
+++ src/main/java/com/deploy/bemyplan/domain/order/repository/OrderRepositoryCustomImpl.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.Objects;
 
 import static com.deploy.bemyplan.domain.order.QOrder.order;
 import static com.deploy.bemyplan.domain.scrap.QScrap.scrap;
@@ -41,21 +42,21 @@ public class OrderRepositoryCustomImpl implements OrderRepositoryCustom{
     }
 
     private BooleanExpression eqUserId(Long userId) {
-        if (userId == null) {
+        if (Objects.isNull(userId)) {
             return null;
         }
         return order.userId.eq(userId);
     }
 
     private BooleanExpression inOrderIds(List<Long> orderIds) {
-        if (orderIds == null) {
+        if (Objects.isNull(orderIds)) {
             return null;
         }
         return order.id.in(orderIds);
     }
 
     private BooleanExpression inPlanIds(List<Long> planIds) {
-        if (planIds == null) {
+        if (Objects.isNull(planIds)) {
             return null;
         }
         return order.planId.in(planIds);

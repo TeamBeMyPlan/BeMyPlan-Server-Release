2ef69b44526e0268ba2266707510a68260390b3e
안준영 <zxzimin@naver.com>
2022-05-12 23:19:47 +0900

    [FIX] parameter modify

--- src/main/java/com/deploy/bemyplan/service/scrap/ScrapService.java
+++ src/main/java/com/deploy/bemyplan/service/scrap/ScrapService.java
@@ -24,7 +24,7 @@ public class ScrapService {
     }
 
     @Transactional
-    public void deleteScrap(Long planId, Long userId){
+    public void deleteScrap(Long userId, Long planId){
         Scrap scrap = scrapRepository.findByUserIdAndPlanId(planId, userId);
         if (scrap == null) {
             throw new NotFoundException(String.format("해당 여행일정을 찜한 상태 (%s - %s) 가 아닙니다.", userId, planId), NOT_FOUND_SCRAP_EXCEPTION);

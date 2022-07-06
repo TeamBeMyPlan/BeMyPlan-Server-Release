9c3082e7f397c41294f333e22e348782e3da47b5
LeeJejune <leejejune0625@gmail.com>
2022-07-06 20:18:09 +0900

    fix: 배포를 위한 run_new_was.sh 수정

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -18,6 +18,6 @@ if [ ! -z ${TARGET_PID} ]; then
   sudo kill ${TARGET_PID}
 fi
 
-nohup java -jar -Dserver.port=${TARGET_PORT} -Dspring.profiles.active=prod /home/ec2-user/bemyplan/build/libs/*.jar > /home/ec2-user/nohup.out 2>&1 &
+nohup java -jar -Dserver.port=${TARGET_PORT} -Dspring.profiles.active=prod /home/ec2-user/bemyplan/application-bemyplan/build/libs/*.jar > /home/ec2-user/nohup.out 2>&1 &
 echo "> Now new WAS runs at ${TARGET_PORT}."
 exit 0

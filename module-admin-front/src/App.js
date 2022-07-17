8c9fd7b611115a47a2d9412cca90efae441acd1c
zkdlu <zkdlu159951@gmail.com>
2022-07-17 19:29:11 +0900

    [front] page 추가

--- module-admin-front/src/App.js
+++ module-admin-front/src/App.js
@@ -1,5 +1,6 @@
 import Topbar from './components/topbar/Topbar'
 import Sidebar from './components/sidebar/Sidebar'
+import Home from './pages/home/Home'
 import './App.css'
 
 function App() {
@@ -8,9 +9,7 @@ function App() {
       <Topbar/>
       <div className='container'>
         <Sidebar/>
-        <div className='page'>
-          page
-        </div>
+        <Home/>
       </div>
     </div>
   );

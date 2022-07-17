a8a3f747931433a2097fcca627f36098f3aea7da
soleu <dlthf555@gmail.com>
2022-07-17 23:52:48 +0900

    [refactor] context-path 제거

--- application-bemyplan/src/main/java/com/deploy/bemyplan/controller/order/OrderController.java
+++ application-bemyplan/src/main/java/com/deploy/bemyplan/controller/order/OrderController.java
@@ -9,12 +9,14 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api")
 public class OrderController {
 
     private final OrderService orderService;

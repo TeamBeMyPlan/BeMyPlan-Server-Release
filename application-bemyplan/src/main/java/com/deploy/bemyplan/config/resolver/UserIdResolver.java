fd9954a654dac412a1681ae2e49759e08726a7e0
soleu <dlthf555@gmail.com>
2022-07-31 00:59:54 +0900

    [fix] 썬 사라져라

--- application-bemyplan/src/main/java/com/deploy/bemyplan/config/resolver/UserIdResolver.java
+++ application-bemyplan/src/main/java/com/deploy/bemyplan/config/resolver/UserIdResolver.java
@@ -1,7 +1,6 @@
 package com.deploy.bemyplan.config.resolver;
 
 import com.deploy.bemyplan.config.interceptor.Auth;
-import com.sun.jdi.InternalException;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
@@ -24,11 +23,12 @@ public class UserIdResolver implements HandlerMethodArgumentResolver {
     @Override
     public Object resolveArgument(@NotNull MethodParameter parameter, ModelAndViewContainer mavContainer, @NotNull NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
         if (parameter.getMethodAnnotation(Auth.class) == null) {
-            throw new InternalException("인증이 필요한 컨트롤러 입니다. @Auth 어노테이션을 붙여주세요.");
+            throw new RuntimeException("인증이 필요한 컨트롤러 입니다. @Auth 어노테이션을 붙여주세요.");
         }
+
         Object object = webRequest.getAttribute(USER_ID, 0);
         if (object == null) {
-            throw new InternalException(String.format("USER_ID를 가져오지 못했습니다. (%s - %s)", parameter.getClass(), parameter.getMethod()));
+            throw new RuntimeException(String.format("USER_ID를 가져오지 못했습니다. (%s - %s)", parameter.getClass(), parameter.getMethod()));
         }
         return object.equals(GUEST_MODE) ? null : object;
     }

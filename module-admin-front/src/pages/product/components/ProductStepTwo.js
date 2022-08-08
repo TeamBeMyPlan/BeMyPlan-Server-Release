b918b55b607cb2ec1d201fa15347ea36cf67b876
zkdlu <zkdlu159951@gmail.com>
2022-08-08 23:48:24 +0900

    front spot grid

--- module-admin-front/src/pages/product/components/ProductStepTwo.js
+++ module-admin-front/src/pages/product/components/ProductStepTwo.js
@@ -6,15 +6,27 @@ import DaumPostcodeEmbed from 'react-daum-postcode';
 import locationApi from '../../../components/locationApi'
 import imageApi from '../../../components/imageApi'
 
+const TableContainer = ({ striped, children }) => (
+    <table className={striped ? "table-striped" : ""}>{children}</table>
+);
+const TableHeader = ({ children }) => <thead>{children}</thead>;
+const TableBody = ({ children }) => <tbody>{children}</tbody>;
+const TableRow = ({ children }) => <tr>{children}</tr>;
+const TableCell = ({ children }) => <td>{children}</td>;
+
 class ProductStepTwo extends Component {
 
     state = {
         openPostCode: false,
+        name: '',
+        review: '',
+        tip: '',
         address: '',
         longitude: 0,
         latitude: 0,
         images: [],
-        savedImages: []
+        savedImages: [],
+        spots: []
     }
 
     togglePost = () => {
@@ -57,16 +69,65 @@ class ProductStepTwo extends Component {
         });
     }
 
-    render() {
-        const { openPostCode,
+    clear = () => {
+        this.setState({
+            openPostCode: false,
+            name: '',
+            review: '',
+            tip: '',
+            address: '',
+            longitude: 0,
+            latitude: 0,
+            images: [],
+            savedImages: [],
+        });
+    }
+
+    addItem = () => {
+        const {
+            name,
+            review,
+            tip,
             address,
             longitude,
-            latitude
+            latitude,
+            spots
+        } = this.state;
+        
+        const newSpot = {
+            name: name,
+            address: address
+        }
+
+        this.setState({
+            spots: [...spots, newSpot]
+        });
+
+        this.clear();
+    }
+
+    handleName = (e) => { this.setState({ name: e.target.value }) }
+    handleReview = (e) => { this.setState({ review: e.target.value }) }
+    handleTip = (e) => { this.setState({ tip: e.target.value }) }
+
+    render() {
+        const { openPostCode,
+            name,
+            review,
+            tip,
+            address,
+            longitude,
+            latitude,
+            spots
         } = this.state;
         const {
+            handleName,
+            handleReview,
+            handleTip,
             searchPlace,
             selectAddress,
             fileChangedHandler,
+            addItem
         } = this;
 
         return (
@@ -74,13 +135,13 @@ class ProductStepTwo extends Component {
                 <div>
                     <h3>여행지 추가</h3>
                     <Inputs msg='여행지 이름'>
-                        <Textbox hint='여행지 이름' />
+                        <Textbox hint='여행지 이름' value={name} onChange={handleName} />
                     </Inputs>
                     <Inputs msg='솔직 후기'>
-                        <Textbox hint='필수 입력' />
+                        <Textbox hint='필수 입력' value={review} onChange={handleReview}/>
                     </Inputs>
                     <Inputs msg='꿀팁'>
-                        <Textbox hint='선택 입력' />
+                        <Textbox hint='선택 입력' value={tip} onChange={handleTip}/>
                     </Inputs>
                     <Inputs msg='여행지 주소'>
                         <Textbox readOnly={true} hint='여행지 주소' value={address} />
@@ -96,6 +157,31 @@ class ProductStepTwo extends Component {
                             name="files"
                             onChange={fileChangedHandler} />
                     </Inputs>
+                    <Button msg="추가" onClick={addItem} />
+                </div>
+                <div>
+                    <h3>여행지</h3>
+                    <TableContainer>
+                        <TableHeader>
+                            <TableRow>
+                                <TableCell>이름</TableCell>
+                                <TableCell>주소</TableCell>
+                                <TableCell>일차</TableCell>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {
+                                spots.map((spot, index) => (
+                                    <TableRow key={index}>
+                                        <TableCell>{spot.name}</TableCell>
+                                        <TableCell>{spot.address}</TableCell>
+                                        <TableCell>1</TableCell>
+                                    </TableRow>        
+                                ))
+                            }
+                        </TableBody>
+                    </TableContainer>
+                  
                 </div>
                 {
                     openPostCode &&

e965ffa29772c026ddda11b2313d3b8784436633
zkdlu <zkdlu159951@gmail.com>
2022-08-08 23:19:50 +0900

    upload to s3

--- application-bemyplan/src/main/java/com/deploy/bemyplan/BemyplanApplication.java
+++ application-bemyplan/src/main/java/com/deploy/bemyplan/BemyplanApplication.java
@@ -2,14 +2,21 @@ package com.deploy.bemyplan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class BemyplanApplication {
 
+	private static final String APPLICATION_LOCATIONS = "spring.config.location=" +
+			"classpath:application.yml," +
+			"classpath:aws.yml";
+
 	public static void main(String[] args) {
-		SpringApplication.run(BemyplanApplication.class, args);
+		new SpringApplicationBuilder(BemyplanApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
 	}
 
 }
--- module-admin-bemyplan/src/main/java/com/deploy/bemyplan/location/LocationApi.java
+++ module-admin-bemyplan/src/main/java/com/deploy/bemyplan/location/LocationApi.java
@@ -2,11 +2,13 @@ package com.deploy.bemyplan.location;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("api")
 public class LocationApi {
 
     private final LocationService locationService;

7894007eeb616f0e8d27ba06ffae091a6d503e76
zkdlu <zkdlu159951@gmail.com>
2022-08-09 23:08:58 +0900

    프론트 3단계 화면

--- module-admin-front/src/pages/product/components/ProductStep3.js
+++ module-admin-front/src/pages/product/components/ProductStep3.js
@@ -1,22 +1,91 @@
 import { Component } from 'react';
 import Button from '../../../components/button/Button';
 import Inputs from '../../../components/inputs/Inputs'
+import Textbox from '../../../components/textbox/Textbox';
+import imageApi from '../../../components/imageApi'
 
 class ProductStep3 extends Component {
 
     state = {
-        
+        previews: [
+            {
+                image: '',
+                description: ''
+            }
+        ]
+    }
+
+    addPreview = () => {
+        const { previews } = this.state;
+        this.setState({
+            previews: [...previews, {
+                image: '',
+                description: ''
+            }]
+        })
+    }
+
+    handleDescription = (e, index) => {
+        this.setState((state) => {
+            const newPreviews = [...state.previews];
+            const prevPreview = newPreviews[index];
+
+            newPreviews[index] = {
+                image: prevPreview.image,
+                description: e.target.value
+            }
+            return {
+                previews: newPreviews
+            }
+        })
+    }
+
+    fileChangedHandler = async (e, index) => {
+        const files = e.target.files;
+        const formData = new FormData();
+
+        for (let i = 0; i < files.length; i++) {
+            formData.append('files', files[i]);
+        }
+
+        const response = await imageApi.upload(formData);
+
+        this.setState((state) => {
+            const newPreviews = [...state.previews];
+            const prevPreview = newPreviews[index];
+
+            newPreviews[index] = {
+                image: response.data[0],
+                description: prevPreview.description
+            }
+            return {
+                previews: newPreviews
+            }
+        })
     }
 
     render() {
+        const { previews } = this.state;
+        const { addPreview,
+            handleDescription,
+            fileChangedHandler } = this;
+
         return (
             <div>
-                <h3>미리 보기</h3>
-                <Inputs msg='여행지 사진'>
-                        <input type="file"
-                            name="file"/>
-                </Inputs>
-                <Button msg="추가"/>
+                {
+                    previews.map((preview, index) => (
+                        <div key={index}>
+                            <h3>미리보기 {index + 1}</h3>
+                            <Inputs msg='여행지 사진'>
+                                <input type="file" name="files" onChange={(e) => fileChangedHandler(e, index)}/>
+                            </Inputs>
+                            <Inputs msg='설명'>
+                                <Textbox hint='미리보기 설명' value={preview.description} onChange={(e) => handleDescription(e, index)} />
+                            </Inputs>
+                        </div>
+                    ))
+                }
+                <Button msg="추가" onClick={addPreview} />
             </div>
         );
     }

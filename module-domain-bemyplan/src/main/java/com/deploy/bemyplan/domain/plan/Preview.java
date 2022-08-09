b6bc1e6fe9c3fd9ca81e1e5370266e39d95693cf
soleu <dlthf555@gmail.com>
2022-08-09 22:18:53 +0900

    [feat] Preview Entity 생성

---
+++ module-domain-bemyplan/src/main/java/com/deploy/bemyplan/domain/plan/Preview.java
@@ -0,0 +1,39 @@
+package com.deploy.bemyplan.domain.plan;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Preview {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "plan_id", nullable = false)
+    private Plan plan;
+
+    @Column(length = 1000)
+    private String imageUrl;
+
+    @Column(length = 2000)
+    private String description;
+
+    @Column(nullable = false, length = 30)
+    @Enumerated(EnumType.STRING)
+    private PreviewContentStatus status;
+}

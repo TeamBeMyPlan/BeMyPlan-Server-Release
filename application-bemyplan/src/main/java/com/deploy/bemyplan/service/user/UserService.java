3c95edde8a943cb385ecda25cfa2d30d3635f11b
LeeJejune <leejejune0625@gmail.com>
2022-08-13 17:53:33 +0900

    feat: 크리에이터 조회 Service 구현 및 ResponseDTO 구현

--- application-bemyplan/src/main/java/com/deploy/bemyplan/service/user/UserService.java
+++ application-bemyplan/src/main/java/com/deploy/bemyplan/service/user/UserService.java
@@ -1,15 +1,23 @@
 package com.deploy.bemyplan.service.user;
 
+import com.deploy.bemyplan.common.exception.model.NotFoundException;
+import com.deploy.bemyplan.domain.plan.Plan;
+import com.deploy.bemyplan.domain.plan.PlanRepository;
 import com.deploy.bemyplan.domain.user.User;
 import com.deploy.bemyplan.domain.user.UserRepository;
 import com.deploy.bemyplan.domain.user.WithdrawalUser;
 import com.deploy.bemyplan.domain.user.WithdrawalUserRepository;
 import com.deploy.bemyplan.service.user.dto.request.CheckAvailableNameRequestDto;
 import com.deploy.bemyplan.service.user.dto.request.CreateUserDto;
+import com.deploy.bemyplan.service.user.dto.response.CreatorInfoResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
+
+import static com.deploy.bemyplan.common.exception.ErrorCode.NOT_FOUND_PLAN_EXCEPTION;
 
 @RequiredArgsConstructor
 @Service
@@ -18,6 +26,7 @@ public class UserService {
     private final UserRepository userRepository;
     private final WithdrawalUserRepository withdrawalUserRepository;
     private final ApplicationEventPublisher eventPublisher;
+    private final PlanRepository planRepository;
 
     @Transactional
     public Long registerUser(CreateUserDto request) {
@@ -28,6 +37,16 @@ public class UserService {
         userRepository.save(user);
 
         return user.getId();
+    }
+
+    @Transactional
+    public CreatorInfoResponse getCreatorInfo(Long planId){
+        Plan plan = planRepository.findPlanById(planId);
+        if (Objects.isNull(plan)) {
+            throw new NotFoundException(String.format("존재하지 않는 일정 (%s) 입니다", planId), NOT_FOUND_PLAN_EXCEPTION);
+        }
+        User user = userRepository.findUserById(plan.getUserId());
+        return CreatorInfoResponse.of(plan.getUserId(), user.getNickname(), plan.getDescription());
     }
 
     @Transactional

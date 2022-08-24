d7f905e934ee0fe580baf73bb7a2f3219c299a6b
zkdlu <zkdlu159951@gmail.com>
2022-08-24 22:28:17 +0900

    Revert "spot image manytoone 제거"
    This reverts commit 5de3dda9c972e27ff338a42b9998a9e987c6e361.

--- module-domain-bemyplan/src/main/java/com/deploy/bemyplan/domain/plan/Spot.java
+++ module-domain-bemyplan/src/main/java/com/deploy/bemyplan/domain/plan/Spot.java
@@ -48,7 +48,6 @@ public class Spot extends AuditingTimeEntity {
     private DailySchedule schedule;
 
     public Spot(Long id, String title, SpotCategoryType category, Location location, String tip, String review,
-                List<SpotImage> images,
                 DailySchedule schedule) {
         this.title = title;
         this.category = category;
@@ -60,8 +59,11 @@ public class Spot extends AuditingTimeEntity {
     }
 
     public Spot(String title, SpotCategoryType category, Location location, String tip, String review,
-                List<SpotImage> images,
                 DailySchedule schedule) {
-        this(null, title, category, location, tip, review, images, schedule);
+        this(null, title, category, location, tip, review, schedule);
+    }
+
+    public void setImage(List<SpotImage> spotImages) {
+        this.images.addAll(spotImages);
     }
 }
--- module-domain-bemyplan/src/main/java/com/deploy/bemyplan/domain/plan/SpotImage.java
+++ module-domain-bemyplan/src/main/java/com/deploy/bemyplan/domain/plan/SpotImage.java
@@ -9,9 +9,12 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -29,8 +32,13 @@ public class SpotImage extends AuditingTimeEntity {
     @Enumerated(EnumType.STRING)
     private SpotImageStatus status;
 
-    public SpotImage(String url) {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "spot_id", nullable = false)
+    private Spot spot;
+
+    public SpotImage(String url, Spot spot) {
         this.url = url;
         this.status = SpotImageStatus.ACTIVE;
+        this.spot = spot;
     }
 }

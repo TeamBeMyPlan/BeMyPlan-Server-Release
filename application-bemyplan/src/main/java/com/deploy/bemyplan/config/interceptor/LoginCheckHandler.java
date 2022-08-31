3a6c5a1826b3e3cca0d8eca673514fe83d12e31c
zkdlu <zkdlu159951@gmail.com>
2022-08-31 22:59:42 +0900

    fix broken guest mode

--- application-bemyplan/src/main/java/com/deploy/bemyplan/config/interceptor/LoginCheckHandler.java
+++ application-bemyplan/src/main/java/com/deploy/bemyplan/config/interceptor/LoginCheckHandler.java
@@ -1,6 +1,5 @@
 package com.deploy.bemyplan.config.interceptor;
 
-import com.deploy.bemyplan.common.exception.model.UnAuthorizedException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.session.Session;
@@ -21,22 +20,20 @@ public class LoginCheckHandler {
 
     Long getUserId(HttpServletRequest request) {
         String sessionId = request.getHeader(HttpHeaders.AUTHORIZATION);
-        if (StringUtils.hasLength(sessionId)) {
-            Session session = findSessionBySessionId(sessionId);
-            Long userId = session.getAttribute(USER_ID);
-            if (userId != null) {
-                return userId;
-            }
+        if (!StringUtils.hasLength(sessionId)) {
+            return TEMP_GUEST_MODE;
+        }
+
+        Session session = sessionRepository.findById(sessionId);
+        if (session == null) {
+            return TEMP_GUEST_MODE;
+        }
+
+        Long userId = session.getAttribute(USER_ID);
+        if (userId != null) {
+            return userId;
         }
 
         return TEMP_GUEST_MODE;
-    }
-
-    private Session findSessionBySessionId(String sessionId) {
-        Session session = sessionRepository.findById(sessionId);
-        if (session == null) {
-            throw new UnAuthorizedException(String.format("잘못된 세션 (%S) 입니다", sessionId));
-        }
-        return session;
     }
 }

05a299c5403cce5730850327c1736c9992b4e406
zkdlu <zkdlu159951@gmail.com>
2023-01-27 22:19:48 +0900

    리팩터링

--- components/module-admin-bemyplan/src/main/java/com/deploy/bemyplan/plan/PreviewAdapter.java
+++ components/module-admin-bemyplan/src/main/java/com/deploy/bemyplan/plan/PreviewAdapter.java
@@ -1,13 +1,20 @@
 package com.deploy.bemyplan.plan;
 
+import com.deploy.bemyplan.domain.plan.JsonValueType;
 import com.deploy.bemyplan.domain.plan.LegacyPreviewRepository;
+import com.deploy.bemyplan.domain.plan.Plan;
 import com.deploy.bemyplan.domain.plan.Preview;
 import com.deploy.bemyplan.domain.plan.PreviewContent;
+import com.deploy.bemyplan.domain.plan.PreviewContentStatus;
 import com.deploy.bemyplan.domain.plan.PreviewRepository;
+import com.deploy.bemyplan.domain.plan.Spot;
+import com.deploy.bemyplan.image.s3.S3Locator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Component
@@ -15,12 +22,29 @@ public class PreviewAdapter {
     private final LegacyPreviewRepository legacyPreviewRepository;
     private final PreviewRepository previewRepository;
 
-    public void saveAll(List<Preview> previews) {
+    public void saveAll(List<PreviewDto> previewDtos, Plan plan, List<Spot> spots) {
+        List<Preview> previews = previewDtos.stream()
+                .map(preview -> Preview.newInstance(plan, List.of(S3Locator.get(preview.getImage())),
+                        preview.getDescription(),
+                        PreviewContentStatus.ACTIVE,
+                        spots.get(preview.getSpotId()).getId()))
+                .collect(Collectors.toList());
+        savePreviews(previews);
+
+        List<PreviewContent> legacyPreviews = new ArrayList<>();
+        previewDtos.forEach(preview -> {
+            legacyPreviews.add(new PreviewContent(plan, JsonValueType.IMAGE, S3Locator.get(preview.getImage())));
+            legacyPreviews.add(new PreviewContent(plan, JsonValueType.TEXT, preview.getDescription()));
+        });
+        saveLegacyPreviews(legacyPreviews);
+    }
+
+    private void savePreviews(List<Preview> previews) {
         previewRepository.saveAll(previews);
     }
 
     @Deprecated
-    public void saveLegacyAll(List<PreviewContent> legacyPreviews) {
+    private void saveLegacyPreviews(List<PreviewContent> legacyPreviews) {
         legacyPreviewRepository.saveAll(legacyPreviews);
     }
 }

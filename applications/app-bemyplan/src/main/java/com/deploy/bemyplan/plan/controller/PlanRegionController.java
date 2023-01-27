e73ef2afb183a3ac6fdff7835210ac82271eb585
zkdlu <zkdlu159951@gmail.com>
2023-01-27 22:13:42 +0900

    리팩터링

--- applications/app-bemyplan/src/main/java/com/deploy/bemyplan/plan/controller/PlanRegionController.java
+++ applications/app-bemyplan/src/main/java/com/deploy/bemyplan/plan/controller/PlanRegionController.java
@@ -1,11 +1,15 @@
 package com.deploy.bemyplan.plan.controller;
 
+import com.deploy.bemyplan.domain.plan.RegionCategory;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
@@ -14,6 +18,9 @@ public class PlanRegionController {
     @ApiOperation("현재 활성화중인 지역 카테고리 목록들을 조회합니다.")
     @GetMapping("/v1/plan/regions")
     public List<RegionTypeResponse> getActiveRegionTypes() {
-        return RegionTypeMapper.getInstance().getActiveRegionTypes();
+        return Arrays.stream(RegionCategory.values())
+                .sorted(Comparator.comparingInt(RegionCategory::getDisplayOrder))
+                .map(RegionTypeResponse::of)
+                .collect(Collectors.toList());
     }
 }

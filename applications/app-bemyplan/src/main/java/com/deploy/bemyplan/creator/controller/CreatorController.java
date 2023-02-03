56258937d1d7f605f6eb269af7775c54cb1b632e
soleu <dlthf555@gmail.com>
2023-02-03 20:55:32 +0900

    refactor : path 변경

--- applications/app-bemyplan/src/main/java/com/deploy/bemyplan/creator/controller/CreatorController.java
+++ applications/app-bemyplan/src/main/java/com/deploy/bemyplan/creator/controller/CreatorController.java
@@ -7,8 +7,8 @@ import com.deploy.bemyplan.user.service.dto.response.CreatorInfoResponse;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,8 +20,8 @@ public class CreatorController {
     private final CreatorService creatorService;
 
     @ApiOperation("크리에이터 조회 - 크리에이터의 정보를 조회합니다.")
-    @GetMapping("/{planId}/creator")
-    public CreatorInfoResponse getCreatorInfo(@PathVariable final Long planId) {
+    @GetMapping("/info")
+    public CreatorInfoResponse getCreatorInfo(@RequestParam("planId") final Long planId) {
         return creatorService.getCreatorInfo(planId);
     }
 

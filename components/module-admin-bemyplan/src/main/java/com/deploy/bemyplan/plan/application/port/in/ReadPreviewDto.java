5afe02860f58e53d4453bf49a0d58e8ebf3d5841
zkdlu <zkdlu159951@gmail.com>
2023-02-16 23:01:01 +0900

    Read plan dto 분리 작업

--- components/module-admin-bemyplan/src/main/java/com/deploy/bemyplan/plan/application/port/in/ReadPreviewDto.java
+++ components/module-admin-bemyplan/src/main/java/com/deploy/bemyplan/plan/application/port/in/ReadPreviewDto.java
@@ -7,12 +7,15 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public class ReadPreviewDto {
+    private final long id;
     private final long spotId;
     private final String description;
     private final String image;
 
     public static ReadPreviewDto from(Preview preview) {
-        return new ReadPreviewDto(preview.getSpot().getId(),
+        return new ReadPreviewDto(
+                preview.getId(),
+                preview.getSpot().getId(),
                 preview.getDescription(),
                 preview.getImageUrls().get(0));
     }

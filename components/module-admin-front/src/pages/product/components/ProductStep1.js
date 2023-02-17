84bd69cb48e5e822978222ec79f491ce1d7d0b17
zkdlu <zkdlu159951@gmail.com>
2023-02-17 15:05:57 +0900

    여행시기 값 타입 변경

--- components/module-admin-front/src/pages/product/components/ProductStep1.js
+++ components/module-admin-front/src/pages/product/components/ProductStep1.js
@@ -103,18 +103,18 @@ class ProductStep1 extends Component {
                     </Inputs>
                     <Inputs msg='여행시기'>
                         <ComboBox selectedValue={this.props.period} items={[
-                            { value: '1', label: '1월' },
-                            { value: '2', label: '2월' },
-                            { value: '3', label: '3월' },
-                            { value: '4', label: '4월' },
-                            { value: '5', label: '5월' },
-                            { value: '6', label: '6월' },
-                            { value: '7', label: '7월' },
-                            { value: '8', label: '8월' },
-                            { value: '9', label: '9월' },
-                            { value: '10', label: '10월' },
-                            { value: '11', label: '11월' },
-                            { value: '12', label: '12월' },
+                            { value: 1, label: '1월' },
+                            { value: 2, label: '2월' },
+                            { value: 3, label: '3월' },
+                            { value: 4, label: '4월' },
+                            { value: 5, label: '5월' },
+                            { value: 6, label: '6월' },
+                            { value: 7, label: '7월' },
+                            { value: 8, label: '8월' },
+                            { value: 9, label: '9월' },
+                            { value: 10, label: '10월' },
+                            { value: 11, label: '11월' },
+                            { value: 12, label: '12월' },
                         ]} onChange={onChangePeriod} />
                     </Inputs>
                     <Inputs msg='가격'>

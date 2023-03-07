271be618e1212845b5264d337492d0478feb4b10
soleu <dlthf555@gmail.com>
2023-03-07 21:27:15 +0900

    예시 템플릿 변경

--- applications/app-bemyplan/src/main/java/com/deploy/bemyplan/temp/response/TempSpotDetailResponse.java
+++ applications/app-bemyplan/src/main/java/com/deploy/bemyplan/temp/response/TempSpotDetailResponse.java
@@ -1,11 +1,12 @@
 package com.deploy.bemyplan.temp.response;
 
+import com.deploy.bemyplan.common.controller.AuditingTimeResponse;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,18 +14,29 @@ import java.util.List;
 @ToString
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-public class TempSpotDetailResponse {
+public class TempSpotDetailResponse extends AuditingTimeResponse {
     private String name;
+    private String address;
     private double latitude;
     private double longitude;
     private String tip;
     private String review;
+
     private List<TempSpotImageResponse> images = new ArrayList<>();
 
-    public static TempSpotDetailResponse of(String name, double latitude, double longitude, String tip, String review, List<TempSpotImageResponse> images){
-        return new TempSpotDetailResponse(name, latitude, longitude, tip, review, images);
-
+    private TempSpotDetailResponse(final String name, final String address, final double latitude, final double longitude, final String tip, final String review, final List<TempSpotImageResponse> images) {
+        this.name = name;
+        this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.tip = tip;
+        this.review = review;
+        this.images = images;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
     }
 
+    public static TempSpotDetailResponse of(String name, String address, double latitude, double longitude, String tip, String review, List<TempSpotImageResponse> images) {
+        return new TempSpotDetailResponse(name, address, longitude, latitude, tip, review, images);
+    }
 }
